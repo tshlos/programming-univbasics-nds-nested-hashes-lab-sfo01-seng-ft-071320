ad791ff41c0d87cb9c9d8824396d67462c7b8a88
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-16 04:53:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -24,6 +24,13 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  
+  people = [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
